f50dd4509c3a821663c0cedec59e8944a5d72721
sahiljadhav7 <jadhavsahil36751@gmail.com>
2026-08-26 19:44:42 +0530

    Initialsed the project and also docker

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
